74a2a8e3fbae7e6daba82ea86e36716cc5803590
Acer Laptop <Acer Laptop@AcerLaptop-PC>
2014-11-03 17:11:32 +0000

    Created superclass for all entities. Currently, all entity classes possess only a coordinate, represented as two integers.

---
+++ src/spaceinvaders/Entities/Entity.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders.Entities;
+
+/**
+ *
+ * @author Acer Laptop
+ */
+public class Entity {
+    private int xPosition;
+    private int yPosition;
+    
+    public Entity() {
+        xPosition = 0;
+        yPosition = 0;
+    }
+    
+    public void setX(int newX) {
+        xPosition = newX;
+    }
+    
+    public void setY(int newY) {
+        yPosition = newY;
+    }
+}
