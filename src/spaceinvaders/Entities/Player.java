ee6ae5a87bbe2f99076c9f1dffac4825fe3cf313
Acer Laptop <Acer Laptop@AcerLaptop-PC>
2014-11-03 17:11:51 +0000

    Created Player subclass of Entity

---
+++ src/spaceinvaders/Entities/Player.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders.Entities;
+
+/**
+ *
+ * @author Acer Laptop
+ */
+public class Player extends Entity{
+    private final String name;
+    
+    public Player(String name) {
+        super();
+        this.name = name;
+    }
+}
