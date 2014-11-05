6b19f06563bcd33a1135a321a49a07bbc519377b
Acer Laptop <Acer Laptop@AcerLaptop-PC>
2014-11-05 17:26:43 +0000

    Defined speed attribute for player

--- src/spaceinvaders/Entities/Player.java
+++ src/spaceinvaders/Entities/Player.java
@@ -11,11 +11,21 @@ package spaceinvaders.Entities;
  */
 public class Player extends Entity{
     private final String name;
+    private int movementVector = 0; //Int because only vector for x needs to be represented.
     
     public Player(String name) {
         super();
         this.name = name;
         this.spriteHeight = 10;
         this.spriteWidth = 20;
+        this.speed = 5;
+    }
+    
+    public void setDirection(int newDirection) {
+        movementVector = newDirection;
+    }
+    
+    public int getDirection() {
+        return movementVector;
     }
 }
