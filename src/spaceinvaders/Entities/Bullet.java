51069efcf65e740030b63beb060a28e3727bfe18
Acer Laptop <Acer Laptop@AcerLaptop-PC>
2014-11-08 12:21:21 +0000

    Added image returning script.

--- src/spaceinvaders/Entities/Bullet.java
+++ src/spaceinvaders/Entities/Bullet.java
@@ -6,6 +6,7 @@
 package spaceinvaders.Entities;
 
 import java.awt.Image;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -14,13 +15,28 @@ import java.awt.Image;
 public class Bullet extends Entity{
     private final int damage;
     private final int direction; // + is down, - is up.
+    private Image currentSkin;
     
     public Bullet(Entity owner, int direction) {
         damage = owner.bulletDmg;
         this.direction = direction;
+        
+        String skinName;
+        if (owner.getClass() == new Enemy().getClass()) { // Might not work...
+            skinName = "Skins/Bullet_Enemy.png";
+        } else if (owner.getClass() == new Player("").getClass()) {
+            skinName = "Skins/Bullet_Player.png";
+        } else {
+            skinName = "Skins/Player.png";
+        }
+        try {
+            currentSkin = ImageIO.read(getClass().getResourceAsStream(skinName));
+	}catch(Exception e){
+            System.err.println(e);
+        }
     }
     
     public Image getImage() {
-        //Todo add image gathering stuff...
+        return currentSkin;
     }
 }
