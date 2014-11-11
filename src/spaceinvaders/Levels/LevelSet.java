7d03244c2f9b29676acdfd9f567215ac011cfc7a
Acer Laptop <Acer Laptop@AcerLaptop-PC>
2014-11-11 17:03:43 +0000

    Game now dependent on stored data.

--- src/spaceinvaders/Levels/LevelSet.java
+++ src/spaceinvaders/Levels/LevelSet.java
@@ -16,6 +16,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import spaceinvaders.LoadingBar;
 
 /**
  *
@@ -26,51 +27,47 @@ public class LevelSet {
     public int numInRow;
     public int numInColumn;
     public int[] rowLevels;
+    private LoadingBar loadPanel;
     
-    public LevelSet(int levelToLoad) {
-        ArrayList fileLines = getFileData2(levelToLoad);
-        totalEnemies = 20;
-        numInRow = 5;
-        numInColumn = 4;
+    public LevelSet(int levelToLoad, LoadingBar loadPanel) {
+        this.loadPanel = loadPanel;
+        loadPanel.increment();
+        ArrayList fileLines = getFileData(levelToLoad);
+        totalEnemies = Integer.parseInt((String) fileLines.get(0));//20
+        numInRow = Integer.parseInt((String) fileLines.get(1));//5
+        numInColumn = Integer.parseInt((String) fileLines.get(2));//4
         rowLevels = new int[numInColumn];
-        rowLevels[0] = 3;
-        rowLevels[1] = 2;
-        rowLevels[2] = 2;
-        rowLevels[3] = 1;
-    }
-    
-    private static ArrayList getFileData(int levelToLoad) {
-        //http://stackoverflow.com/questions/2271926/how-to-read-a-file-from-a-jar-file
-        //http://stackoverflow.com/questions/3369794/how-to-a-read-file-from-jar-in-java
-        ArrayList finalLines = new ArrayList();
-        String fileName = "/spaceinvaders/Levels/Level_" + Integer.toString(levelToLoad) + ".txt";
-        Path filePath = Paths.get(fileName);
-        Charset charset = Charset.forName("US-ASCII");
-        try (BufferedReader reader = Files.newBufferedReader(filePath, charset)) {
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                 System.out.println(line);
-                 finalLines.add(line);
-            }
-        } catch (IOException x) {
-            System.err.format("IOException: %s%n", x);
+        //rowLevels[0] = 3;
+        //rowLevels[1] = 2;
+        //rowLevels[2] = 2;
+        //rowLevels[3] = 1;
+        int i;
+        int line = 3;
+        for (i = 0; i <= numInColumn-1 ; i++) {
+            rowLevels[i] = Integer.parseInt((String) fileLines.get(line));
+            line++;
         }
-        return finalLines;
     }
     
-    private ArrayList getFileData2 (int levelToLoad) {
+    private ArrayList getFileData (int levelToLoad) {
         //http://tutorials.jenkov.com/java-io/inputstream.html
         ArrayList finalLines = new ArrayList();
         String fileName = "Level_" + Integer.toString(levelToLoad) + ".txt";
         InputStream input = getClass().getResourceAsStream(fileName);
         Reader reader = new InputStreamReader(input);
+        String asString = "";
+        String[] lineArray;
         try {
             int data = reader.read();
             while(data != -1){
                 char dataChar = (char) data;
-                System.out.println(dataChar);
-                finalLines.add(dataChar);
+                asString += dataChar;
+                //System.out.println(dataChar);
                 data = reader.read();
+            }
+            lineArray = asString.split("\\r?\\n");
+            for (String line : lineArray) {
+                finalLines.add(line);
             }
         } catch (IOException x) {
             System.err.format("IOException: %s%n", x);
