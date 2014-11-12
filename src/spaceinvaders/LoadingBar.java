6ada85b20cb23b682d60c3e411e6e8d265a55ce1
nobesj <nobesj@RDP03.burgate.internal>
2014-11-12 12:48:26 +0000

    LoadingBar can now show some text.

--- src/spaceinvaders/LoadingBar.java
+++ src/spaceinvaders/LoadingBar.java
@@ -19,6 +19,7 @@ public class LoadingBar extends JPanel{
     private int totalLength = 200;
     private float totalItems = 28;
     private float currentItem = 0;
+    private String displayText = "";
     
     @Override
     public void paint(Graphics g) {
@@ -26,8 +27,11 @@ public class LoadingBar extends JPanel{
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                                 RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setBackground(Color.DARK_GRAY);
         g2d.setColor(Color.blue);
         g2d.fillRect(0, 0, (int) ((currentItem/totalItems) * totalLength), 50);
+        g2d.setColor(Color.black);
+        g2d.drawString(displayText,70,25);
         if (currentItem == totalItems) {
             System.out.println("Loading done.");
         }
@@ -42,5 +46,12 @@ public class LoadingBar extends JPanel{
         }
         currentItem += 1;
         repaint();
+    }
+    
+    public LoadingBar() {
+    }
+    
+    public LoadingBar(String text) {
+        displayText = text;
     }
 }
