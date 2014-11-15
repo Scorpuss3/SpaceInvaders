79dca8465e0fc4577b65c4ce5b1da674e0c3083d
Acer Laptop <Acer Laptop@AcerLaptop-PC>
2014-11-15 17:02:31 +0000

    HUD JPanel for graphics has been added to keep track of player progress.

---
+++ src/spaceinvaders/HUD.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.JPanel;
+import spaceinvaders.Entities.*;
+
+/**
+ *
+ * @author Acer Laptop
+ */
+public class HUD extends JPanel {
+    private Player hudPlayer;
+    private final SpaceInvaders session;
+    
+    @Override
+        public void paint(Graphics g) {
+            super.paint(g);
+            SpaceInvaders freezeFrame = session;
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            g2d.fillRect(0,0,this.getWidth(),this.getHeight());
+            g2d.setColor(Color.red);
+            g2d.drawString("Health: " + Integer.toString(session.player.getHealth()),((float)this.getWidth() / 5)*0.5f,this.getHeight()/2);
+            g2d.drawString("Score: " + Integer.toString(session.player.getScore()),(this.getWidth() / 5)*1.5f,this.getHeight()/2);
+            g2d.drawString("Level: " + Integer.toString(SpaceInvaders.level),((float)this.getWidth() / 5)*2.5f,this.getHeight()/2);
+        }
+    
+    public HUD(SpaceInvaders parsedSession,int width,int height) {
+        session = parsedSession;
+        this.setSize(width,height);
+    }
+}
